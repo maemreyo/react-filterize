f7843e863e4e3e81ac09440d76c74ebf44b47a74
Matthew Ngo <ngonhuthanhtrung1409@gmail.com>
2025-01-16 00:07:18 +0700

    chore: change expose api

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
-// input/src/index.ts
 export { useFilterize } from './hooks/useFilterize';
-export { useQueryFilter } from './hooks/useQueryFilter';
-export { useRangeFilter } from './hooks/useRangeFilter';
-export { useSelectFilter } from './hooks/useSelectFilter';
-export { useFilterAnalytics } from './hooks/useFilterAnalytics';
 export * from './types';
 export * from './storage/types';
-export { MemoryStorageAdapter } from './storage/adapters/memoryStorageAdapter';
-export { LocalStorageAdapter } from './storage/adapters/localStorageAdapter';
-export { SessionStorageAdapter } from './storage/adapters/sessionStorageAdapter';
-export { StorageManager } from './storage/adapters/storageManager';
-export { serializeFilters, deserializeFilters } from './utils/serialization';
-export { validateFilters } from './utils/validation';
-export { getPresetFilters } from './utils/presets';
