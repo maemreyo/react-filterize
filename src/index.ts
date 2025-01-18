31c55690569906bcfa9e2835a93ddafa319fea67
Matthew Ngo <ngonhuthanhtrung1409@gmail.com>
2025-01-18 11:40:42 +0700

    chore: update types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,4 @@ export { useFilterize } from './hooks/useFilterize';
 export * from './types';
 export * from './types/storage';
 export * from './types/fetch';
-export * from './utils/typing';
-export * from './utils/url';
-export * from './utils/dependency';
+export * from './types/url';
